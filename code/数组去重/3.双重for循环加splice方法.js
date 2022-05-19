5418cf9d8e49d2fbd239029935aceb8bf7eb9e40
yefanglei <1115326582@qq.com>
2022-05-19 21:19:01 +0800

    fix

--- code/数组去重/3.双重for循环加splice方法.js
+++ code/数组去重/3.双重for循环加splice方法.js
@@ -3,7 +3,7 @@ const norepeate = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] === arr[j]) {
-        arr.splice(i, 1)
+        arr.splice(j, 1)
         j--
       }
     }
